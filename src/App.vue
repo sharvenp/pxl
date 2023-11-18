10317b41eb59c96c933f52d34c357d2aceaac449
sharvenp <sharvenprasad101@gmail.com>
2023-11-17 22:32:11 -0500

    add backlog to gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <h1>Test</h1>
-  <button @click="ping">Test</button>
 </template>
 
 <script setup lang="ts">
-import { ipcAPI } from './ipc-api';
-
-function ping() {
-  ipcAPI("DEBUG_PING")
-}
-
 </script>
