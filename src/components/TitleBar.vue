4434ea5218c86d054e0fff7bd38a3b4e95692d04
sharvenp <sharvenprasad101@gmail.com>
2025-03-28 22:38:42 -0400

    add menu dropdowns

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="title-bar absolute w-full grid grid-rows-2 pb-1 z-10">
+    <div class="title-bar absolute w-full grid grid-rows-2 pb-1 z-20">
         <div class="mt-1 flex flex-row text-sm justify-between">
             <span class="text-sm font-bold px-2">.pxl</span>
             <!-- TODO: only show this if in desktop mode~~~ -->
@@ -9,41 +9,35 @@
                 <button class="px-2 self-center bg-stone-300 hover:bg-stone-400" @click="">X</button>
             </div>
         </div>
-        <div class="mt-1 flex flex-row text-sm">
-            <button class="mx-2 px-2 self-center bg-stone-300 hover:bg-stone-400" @click="openMenu('File')">File</button>
-            <button class="me-1 px-2 self-center bg-stone-300 hover:bg-stone-400" @click="openMenu('Edit')">Edit</button>
-            <button class="me-1 px-2 self-center bg-stone-300 hover:bg-stone-400" @click="openMenu('View')">View</button>
-            <button class="px-2 self-center bg-stone-300 hover:bg-stone-400" @click="openMenu('Help')">Help</button>
+        <div class="mt-1 flex flex-row text-sm relative">
+            <div v-for="(options, menu) in menuOptions" :key="menu" class="relative mx-1">
+                <button class="px-2 self-center bg-stone-300 hover:bg-stone-400" @click="openMenu(menu)">{{ menu }}</button>
+                <div v-if="activeMenu === menu" class="absolute bg-white border mt-1 shadow-md w-max">
+                    <ul>
+                        <li v-for="option in options" :key="option" class="px-4 py-1 hover:bg-stone-200 cursor-pointer text-xs">
+                            {{ option }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, inject, onUnmounted } from 'vue'
+import { ref, inject } from 'vue'
 import { InstanceAPI } from '../api';
-import { Events, GridMouseEvent } from '../api/utils';
 
 const iApi = inject<InstanceAPI>('iApi');
-let handlers: Array<string> = [];
+const activeMenu = ref<string | undefined>(undefined);
+const menuOptions = ref({
+    File: ['New Project', 'Open Project', 'Save', 'Save As', 'Exit'],
+    Edit: ['Undo', 'Redo'],
+    View: ['Zoom In', 'Zoom Out', 'Reset View'],
+    Help: ['About']
+});
 
-onMounted(() => {
-    // handlers.push(iApi?.event.on(Events.MOUSE_MOVE, (evt: GridMouseEvent) => {
-    //     x.value = Math.floor(evt.coords.x) + 1;
-    //     y.value = Math.floor(evt.coords.y) + 1;
-    //     isOnCanvas.value = true;
-    // })!);
-
-    // handlers.push(iApi?.event.on(Events.CANVAS_MOUSE_LEAVE, () => {
-    //     isOnCanvas.value = false;
-    // })!);
-})
-
-onUnmounted(() => {
-    handlers.forEach(h => iApi?.event.off(h));
-})
-
-function openMenu(x: string) {
-    console.log('open',x);
+function openMenu(menu: string) {
+    activeMenu.value = activeMenu.value === menu ? undefined : menu;
 }
-
 </script>
